24b452aa8d29bf4874fdb4c77cc3fddb710b3e69
Stefano Antonini <steanthos@gmail.com>
2023-01-28 14:21:41 +0100

    Solve almost every problem

--- _site/js/script.js
+++ _site/js/script.js
@@ -4,3 +4,6 @@ const navLinks = document.getElementsByClassName('nav-links')[0]
 navToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active')
 })
+navLinks.addEventListener('click', () =>{
+  navLinks.classList.remove('active')
+})
